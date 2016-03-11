9318d6715f5f51481c88511e926cbf65a7316622
Mike Goren <mgoren@mortalwombat.net>
2016-03-11 11:54:06 -0800

    add list-task child component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,17 +1,42 @@
 import { Component } from 'angular2/core';
 
+// task-list component
+
+@Component({
+  selector: 'task-list',
+  inputs: ['tasks'],
+  template: `
+    <h3 *ngFor="#task of tasks" (click)="taskClicked(task)">
+      {{task.description}}
+    </h3>
+  `
+})
+
+export class TaskListComponent {
+  public tasks: Task[];
+  taskClicked(task: Task): void {
+    console.log(task);
+  }
+}
+
+
+// my-app component
+
 @Component({
   selector: 'my-app',
+  directives: [TaskListComponent],
   template: `
     <div class = "container">
       <h1>Todo List</h1>
-      <h3 *ngFor="#task of tasks">{{ task.description }}</h3>
+      <h3 (click)="showMessage">
+        <task-list [tasks]="tasks"></task-list>
+      </h3>
     </div>
   `
 })
 
 export class AppComponent {
-  public tasks: Task[];
+  public tasks: Array<Task>;
   constructor() {
     this.tasks = [
       new Task("Create To-Do List app.", 0),
@@ -20,10 +45,16 @@ export class AppComponent {
       new Task("Do the laundry.", 3)
     ];
   }
+  showMessage() {
+    console.log("click received by my-app component");
+  }
 }
+
+
+
+// model definition
 
 export class Task {
   public done: boolean = false;
-  constructor(public description: string, public id: number) {
-  }
+  constructor(public description: string, public id: number) {}
 }
